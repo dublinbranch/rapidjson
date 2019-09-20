8143676ef4e20007d81facca158cb5136a7d474b
Roy Bellingan <roy@ech.chameleon.ad>
2019-09-20 16:41:20 +0100

    small note

--- includeMe.h
+++ includeMe.h
@@ -3,7 +3,10 @@
 
 void rapidAssert(bool condition);
 #define RAPIDJSON_ASSERT(x) rapidAssert(x); //Do not call ABORT, call our custom assert which has inside the stack unwid via Backward
+
+//this will require QMAKE_CXXFLAGS += -msse4.2
 #define RAPIDJSON_SSE42 1 //why not ? we do not target gameboy anymore
+
 #define RAPIDJSON_48BITPOINTER_OPTIMIZATION 1 //stuff some data in the high part of 64bit ptr (x64 IGNORE after bit 48) 
 #define RAPIDJSON_PARSE_DEFAULT_FLAGS 32 //add the ability to have comment inside 
 
