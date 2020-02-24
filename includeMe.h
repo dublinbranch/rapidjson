32d880cf97feffafff8b1d2d18b9e06d856f135c
Roy Bellingan <roy@tech.techadsmedia.com>
2020-02-24 01:37:04 +0000

    Milo Opinion

--- includeMe.h
+++ includeMe.h
@@ -4,6 +4,19 @@
 void rapidAssert(bool condition);
 #define RAPIDJSON_ASSERT(x) rapidAssert(x); //Do not call ABORT, call our custom assert which has inside the stack unwid via Backward
 
+/**
+void rapidAssert(bool condition) {
+	if (!condition && rapidAssertEnabled) {
+		//qCritical().noquote() << QStacker();
+		throw 3;
+	}
+}
+*/
+
+//Choose to throw on error or not https://github.com/Tencent/rapidjson/issues/1606
+inline thread_local bool rapidAssertEnabled = true;
+
+
 //this will require QMAKE_CXXFLAGS += -msse4.2
 #define RAPIDJSON_SSE42 1 //why not ? we do not target gameboy anymore
 
@@ -13,6 +26,7 @@ void rapidAssert(bool condition);
 #include "rapidjson/document.h"
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
+
 
 
 #endif // RAPIDJSON_INCLUDEME_H
