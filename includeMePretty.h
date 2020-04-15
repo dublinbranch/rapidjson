428d0013570a64309de4328ce4ac6a790f4bcd50
Roy Bellingan <roy@tech.techadsmedia.com>
2020-04-16 00:55:19 +0100

    Cry

--- includeMePretty.h
+++ includeMePretty.h
@@ -4,7 +4,7 @@
 void rapidAssert(bool condition);
 #define RAPIDJSON_ASSERT(x) rapidAssert(x); //Do not call ABORT, call our custom assert which has inside the stack unwid via Backward
 #define RAPIDJSON_SSE42 1 //why not ? we do not target gameboy anymore
-#define RAPIDJSON_48BITPOINTER_OPTIMIZATION 1 //stuff some data in the high part of 64bit ptr (x64 IGNORE after bit 48) 
+#define RAPIDJSON_48BITPOINTER_OPTIMIZATION 1 //stuff some data in the high part of 64bit ptr (x64 IGNORE after bit 48), probably useless but super cool!
 #define RAPIDJSON_PARSE_DEFAULT_FLAGS 32 //add the ability to have comment inside 
 
 #include "rapidjson/document.h"
