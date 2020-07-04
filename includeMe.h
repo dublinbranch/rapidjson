0ea7c6af2e04b2042f149bb3dbd300edc0e1c0e1
Roy Bellingan <roy@tech.techadsmedia.com>
2020-07-04 02:57:29 +0100

    ResetAfterUse

--- includeMe.h
+++ includeMe.h
@@ -15,6 +15,27 @@ void rapidAssert(bool condition) {
 }
 */
 
+/**
+ *small helper class used to set and reset to initial value call like
+ *ResetAfterUse r{rapidAssertEnabled, false}; 
+ */
+template <typename K>
+class ResetAfterUse {
+      public:
+	ResetAfterUse(K& key, const K& value) {
+		old       = key;
+		key       = value;
+		this->key = &key;
+	}
+	~ResetAfterUse() {
+		*key = old;
+	}
+
+      private:
+	K* key;
+	K  old;
+};
+
 //Choose to throw on error or not https://github.com/Tencent/rapidjson/issues/1606
 inline thread_local bool rapidAssertEnabled = true;
 inline thread_local bool rapidAssertPrintTrace = true;
